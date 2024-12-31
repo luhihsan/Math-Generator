446f91a290ba88359705f2da46944e26cc7ab8b8
wrx_365 <93203737+luhihsan@users.noreply.github.com>
2024-12-31 17:36:37 +0700

    update pubsec

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Mathematics',
+      title: 'Smart Math',
       theme: ThemeData(visualDensity: VisualDensity.adaptivePlatformDensity),
       home: const HomeScreen(),
     );
